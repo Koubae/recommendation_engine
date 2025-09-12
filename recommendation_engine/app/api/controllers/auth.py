f42861e89f50fc87affac675a12e2f7f6ba770ab
koubae <58447627+Koubae@users.noreply.github.com>
2025-09-12 16:32:20 +0200

    AuthController (login endpoint)

---
+++ recommendation_engine/app/api/controllers/auth.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+from pydantic import BaseModel, Field
+
+from recommendation_engine.settings import Settings
+
+
+class LoginRequest(BaseModel):
+    username: str = Field(..., min_length=3, max_length=255)
+    password: str = Field(..., min_length=1, repr=False)
+
+
+class LoginResponse(BaseModel):
+    access_token: str
+    expires: float
+
+
+class AuthController:
+    def __init__(self) -> None:
+        self.router: APIRouter = APIRouter()
+        self.settings: Settings = Settings.get()
+        self._register_routes()
+
+    def _register_routes(self) -> None:
+        self.router.add_api_route(path="/login", endpoint=self.login, methods=["POST"])
+
+    @staticmethod
+    async def login(
+        request: LoginRequest,
+    ) -> LoginResponse:
+        print(request)
+        response = LoginResponse(access_token="dummy", expires=0.0)
+        return response
--- recommendation_engine/app/api/router.py
+++ recommendation_engine/app/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from recommendation_engine.app.api.controllers.auth import AuthController
 from recommendation_engine.app.api.controllers.index import IndexController
 
 __all__ = ("get_router",)
@@ -10,4 +11,11 @@ def get_router() -> APIRouter:
 
     index_controller = IndexController()
     router.include_router(index_controller.router, tags=["Index"])
+
+    api_v1 = APIRouter(prefix="/api/v1")
+
+    auth_controller = AuthController()
+    api_v1.include_router(auth_controller.router, prefix="/auth", tags=["Auth"])
+
+    router.include_router(api_v1)
     return router
