e0a42de3d8b480cd4e6b154680d850d895c12af2
koubae <58447627+Koubae@users.noreply.github.com>
2025-09-12 15:31:50 +0200

    Added realistic test

--- tests/recommendation/test_algorithm.py
+++ tests/recommendation/test_algorithm.py
@@ -11,3 +11,42 @@ class TestRecommendationAlgorithms:
     def test_generate_recommendation_subsequences(self, input_ids: tuple[int, ...], expected: list[list[int]]):
         recommendations = generate_recommendation_subsequences(input_ids)
         assert expected == recommendations
+
+    def test_generate_recommendation_subsequences_realistic_case(self):
+        product_ids = sorted((1234, 33912, 30324, 13034, 240234))
+
+        recommendations = generate_recommendation_subsequences(product_ids)
+        expected = [
+            [1234],
+            [13034],
+            [30324],
+            [33912],
+            [240234],
+            [1234, 13034],
+            [1234, 30324],
+            [1234, 33912],
+            [1234, 240234],
+            [13034, 30324],
+            [13034, 33912],
+            [13034, 240234],
+            [30324, 33912],
+            [30324, 240234],
+            [33912, 240234],
+            [1234, 13034, 30324],
+            [1234, 13034, 33912],
+            [1234, 13034, 240234],
+            [1234, 30324, 33912],
+            [1234, 30324, 240234],
+            [1234, 33912, 240234],
+            [13034, 30324, 33912],
+            [13034, 30324, 240234],
+            [13034, 33912, 240234],
+            [30324, 33912, 240234],
+            [1234, 13034, 30324, 33912],
+            [1234, 13034, 30324, 240234],
+            [1234, 13034, 33912, 240234],
+            [1234, 30324, 33912, 240234],
+            [13034, 30324, 33912, 240234],
+            [1234, 13034, 30324, 33912, 240234]
+        ]
+        assert expected == recommendations
